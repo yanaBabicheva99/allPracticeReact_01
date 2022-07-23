5f471f4652badc62ba528b00d830765de32ceac9
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-07-23 15:43:49 +0300

    change pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import _ from 'lodash';
 const Pagination = ({countPosts, sizePage, changeCurrent, current}) => {
 const countPages = Math.ceil(countPosts/sizePage);
-    // if (countPages === 1) {
-    //     return null;
-    // }
+    if (countPages === 1) {
+        return null;
+    }
 const arrPages = _.range(1, countPages + 1);
 
     return (
         <nav className='m-2'>
             <ul className="pagination">
                 {arrPages.map(page =>
-                    <li onClick={() => changeCurrent(page)}
+                    <li
+                        onClick={() => changeCurrent(page)}
                         key={'page'+ page}
-                        className="page-item">
+                        className={'page-item ' + (page === current ? 'active': '')}>
                         <a className="page-link">
                             {page}
                         </a>
