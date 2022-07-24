ee0af4c0195bc8fb4ffc916f5970bfa1fbf81ad4
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-07-24 20:12:01 +0300

    Create API and Loader

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 
 import './Style/App.css';
-import React, {useRef, useState, useMemo} from "react";
+import React, {useRef, useState, useMemo, useEffect} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/postFilter";
 import MyModal from "./components/MyModal/MyModal";
 import MyButton from "./components/UI/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/Loader/Loader";
 function App() {
     const [posts, setPosts] = useState([
-        {id:1, title:'JavaScript', body:'мультипарадигменный язык программирования.' +
-                ' Поддерживает объектно-ориентированный, императивный и функциональный стили.'},
-        {id:2, title:'Python', body:'высокоуровневый язык программирования общего ' +
-                'назначения с динамической строгой типизацией и автоматическим управлением памятью'},
-        {id:3, title:'C++', body:'это компилируемый язык общего назначения, который' +
-                ' поддерживает различные парадигмы программирования, в том числе процедурное' +
-                ' программирование, объектно-ориентированное программирование и обобщённое программирование. '}
+        // {id:1, title:'JavaScript', body:'мультипарадигменный язык программирования.' +
+        //         ' Поддерживает объектно-ориентированный, императивный и функциональный стили.'},
+        // {id:2, title:'Python', body:'высокоуровневый язык программирования общего ' +
+        //         'назначения с динамической строгой типизацией и автоматическим управлением памятью'},
+        // {id:3, title:'C++', body:'это компилируемый язык общего назначения, который' +
+        //         ' поддерживает различные парадигмы программирования, в том числе процедурное' +
+        //         ' программирование, объектно-ориентированное программирование и обобщённое программирование. '}
     ])
 
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [visible, setVisible] = useState(false);
+    const [isPostsLoading, setPostsLoading] = useState(false);
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setVisible(false);
+    }
+
+    async function fetchPosts() {
+        setPostsLoading(true);
+       setTimeout(async() => {
+           const posts = await PostService.getAll();
+           setPosts(posts);
+           setPostsLoading(false);
+           }, 1000)
     }
 
     const removePost = (post) => {
@@ -31,10 +48,12 @@ function App() {
 
     }
 
-    const sortedAndSearchedPost = usePosts(filter.sort, posts, filter.query)
+    const sortedAndSearchedPost = usePosts(filter.sort, posts, filter.query);
+
 
   return (
     <div className="App">
+        <MyButton onClick={() => fetchPosts()}>Get posts</MyButton>
         <MyButton onClick={() => setVisible(true)}>
             Создать пост
         </MyButton>
@@ -46,7 +65,15 @@ function App() {
         </MyModal>
         <hr className='m-2' />
         <PostFilter filter={filter} setFilter={setFilter}/>
-        <PostList posts={sortedAndSearchedPost} title='Список постов 1' onDelete={removePost}/>
+        {
+            isPostsLoading
+                ? <div style={{display:'flex', justifyContent: 'center', marginTop: '50px'}}
+                >
+                    <Loader />
+                 </div>
+            : <PostList posts={sortedAndSearchedPost} title='Список постов 1' onDelete={removePost}/>
+        }
+
     </div>
   );
 }
