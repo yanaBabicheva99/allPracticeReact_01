0c572ee092aff3f3f9ff3f77bc5b28c8950ce11f
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-07-24 15:59:22 +0300

    add usePosrsHook

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import {useMemo} from "react";
 
-export const useSortedPosts = (posts, sort) => {
+export const useSortedPosts = (sort, posts) => {
+
     const sortedPost = useMemo(() => {
         if (sort) {
             return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]));
         }
         return posts;
     }, [sort, posts]);
-
-    return sortedPost;
-
+  return sortedPost;
 }
 
-export const usePosts =(posts, sort, query) => {
-    const sortedPost = useSortedPosts(posts, sort)
+export const usePosts = (sort, posts, query) => {
+    const sortedPost = useSortedPosts(sort, posts)
     const sortedAndSearchedPost = useMemo(() => {
         return sortedPost.filter(post => post.title.toLowerCase().includes(query.toLowerCase()));
     }, [query, sortedPost]);
+
     return sortedAndSearchedPost;
 }
+
